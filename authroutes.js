19d7d86a7c724ff1e743cbe6c65201f002a4aae2
vivekdas-kiit <154360992+harryson22102004@users.noreply.github.com>
2026-03-16 16:40:35 +0530

    Create authroutes.js

---
+++ authroutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { body } = require('express-validator');
+const { register, login, getMe, logout } = require('../controllers/authController');
+const { protect } = require('../middleware/auth');
+
+const router = express.Router();
+
+router.post('/register', [
+  body('name').notEmpty().withMessage('Name is required'),
+  body('email').isEmail().withMessage('Please enter a valid email'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+], register);
+
+router.post('/login', [
+  body('email').isEmail().withMessage('Please enter a valid email'),
+  body('password').notEmpty().withMessage('Password is required')
+], login);
+
+router.get('/me', protect, getMe);
+router.post('/logout', protect, logout);
+
+module.exports = router;
